a4955fc7a21c0b49c900e74f237f521ebba25e46
unknown <liran.markin@gmail.com>
2014-12-21 04:04:51 +0200

    add X 0 shapes

--- XO_files/game.js
+++ XO_files/game.js
@@ -4,11 +4,27 @@ app.controller('gameController',function() {
     this.turn = 1;
     this.board = [0,0,0,0,0,0,0,0,0];
 
+	this.display = function(cell){
+		switch(this.board[cell]){
+			case 0:
+				return 'C';
+				break;
+			case 1:
+				return 'X';
+				break;
+			case 2:
+				return 'O';
+				break;
+			default:
+				return 'F';
+		}
+	};
+
     this.move = function(cell){
 	if(this.board[cell] != 0)
 		return false;
 	this.board[cell] = this.turn;
 	this.turn = 3-this.turn;
-	}
+	};
 });
 
